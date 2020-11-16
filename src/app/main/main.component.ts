e303db2af288019c8d3f30b44269c690a2b5ee74
Shemesh, Ofir <ofir_shemesh@bmc.com>
2020-11-16 15:00:58 +0200

    cleanup

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -6,14 +6,7 @@ import {ApplicationRef, Component, OnInit} from '@angular/core';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
-  private showPopOutP = false;
-  get showPopOut(): boolean {
-    return this.showPopOutP;
-  }
 
-  set showPopOut(value: boolean) {
-    this.showPopOutP = value;
-  }
   public someJah = '321';
 
   constructor(private appRef: ApplicationRef) { }
